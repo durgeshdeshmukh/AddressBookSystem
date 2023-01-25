6ec1b698d2545668897d6735a7d300882e192671
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-24 20:18:00 -0800

    creating contacts in address book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to Address Book!");
+            List<Contacts> ContactsList = new List<Contacts>();
+            ContactsList.Add(new Contacts("Mohan", "sharma", "Nehru Nagar", "Indore", "Madhya Pradesh", 246413165, "mohan@gmail.com"));
         }
     }
 }
