155f792344f1503bc1d7933ceccdef03ad9d17b5
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-26 21:05:29 -0800

    Add person detail using console

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AddressBookSystem
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using AddressBookSystem;
 
-        }
-    }
+Console.WriteLine("Welcome to Address Book!");
+//List<Contacts> ContactsList = new List<Contacts>();
+//ContactsList.Add(new Contacts("Mohan", "sharma", "Nehru Nagar", "Indore", "Madhya Pradesh", 246413165, "mohan@gmail.com"));
+//foreach (Contacts item in ContactsList)
+//    Console.WriteLine(item);
+
+Console.WriteLine("FirstName:");
+string FirstName = Console.ReadLine();
+Console.WriteLine("LastName:");
+string LastName = Console.ReadLine();
+Console.WriteLine("Address:");
+string Address = Console.ReadLine();
+Console.WriteLine("City:");
+string City = Console.ReadLine();
+Console.WriteLine("State:");
+string State = Console.ReadLine();
+Console.WriteLine("PhoneNumber:");
+string PhoneNumber = Console.ReadLine();
+Console.WriteLine("Email:");
+string Email = Console.ReadLine();
+
+List<Contacts> ContactsList = new List<Contacts>();
+ContactsList.Add(new Contacts(FirstName, LastName, Address, City, State, PhoneNumber, Email));
+foreach (var contact in ContactsList)
+{
+    Console.WriteLine(contact);
 }
---
+++ AddressBookSystem/UC2_AddNewContact.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Contacts
+    {
+        public Contacts(string FirstName, string LastName, string Address, string City, string State, string PhoneNumber, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+        }
+        public string FirstName
+        {
+            get { return FirstName; }
+            set { FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return LastName; }
+            set { LastName = value; }
+        }
+        public string Address
+        {
+            get { return Address; }
+            set { Address = value; }
+        }
+        public string City
+        {
+            get { return City; }
+            set { City = value; }
+        }
+        public string State
+        {
+            get { return State; }
+            set { State = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return PhoneNumber; }
+            set { PhoneNumber = value; }
+        }
+        public string Email
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+    }
+}
